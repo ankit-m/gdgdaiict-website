d5be0487c8e79699a419bba219ccb802dee3d776
KhushilMitry <khushil324@gmail.com>
2016-09-10 23:12:16 +0530

    DevFest Page(Sorry for All changes in single commit-Long story)

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
-(function($){
-  $(function(){
-
+(function ($) {
+  $(function () {
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.modal-trigger').leanModal();
   }); // end of document ready
-})(jQuery); // end of jQuery name space
+})(jQuery);// end of jQuery name space
